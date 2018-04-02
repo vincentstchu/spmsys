915b9f9f43a4fc2572ed256534a54e29ba9dca15
vincent <2718904285@qq.com>
2018-04-02 22:44:13 +0800

    corsFilter fix

--- src/main/java/com/ujs/spmsys/filter/CorsFilter.java
+++ src/main/java/com/ujs/spmsys/filter/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter implements Filter{
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token,x-requested-with");
         System.out.println("*********************************过滤器被使用**************************");
         chain.doFilter(req, res);
     }
