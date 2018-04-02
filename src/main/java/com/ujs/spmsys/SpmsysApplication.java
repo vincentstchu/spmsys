03cd47a9c1377ab9b613544b6579b80afce68096
vincent <vincentstchu@outlook.com>
2018-04-02 20:30:03 +0800

    Revert "corsFilter跨域问题"
    This reverts commit 0821f8ebf821d070784c9801aa547ce9d7233efa.

--- src/main/java/com/ujs/spmsys/SpmsysApplication.java
+++ src/main/java/com/ujs/spmsys/SpmsysApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @MapperScan("com.ujs.spmsys.dao")
@@ -20,20 +17,4 @@ public class SpmsysApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpmsysApplication.class, args);
 	}
-
-	//尝试解决拦获取axios请求失败的问题
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        return corsConfiguration;
-    }
-
-    public CorsFilter corsFilter() {
-        logger.info("==========设置跨域过滤器=========");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",buildConfig());
-        return new CorsFilter(source);
-    }
 }
