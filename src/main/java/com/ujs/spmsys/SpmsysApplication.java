231280bf8b9b8c51c6bf05c1b57f99df88f5a5ec
vincent <2718904285@qq.com>
2018-04-02 11:04:01 +0800

    make springboot project run in the tomcat

--- src/main/java/com/ujs/spmsys/SpmsysApplication.java
+++ src/main/java/com/ujs/spmsys/SpmsysApplication.java
@@ -3,10 +3,16 @@ package com.ujs.spmsys;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.ujs.spmsys.dao")
-public class SpmsysApplication {
+public class SpmsysApplication extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(SpmsysApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpmsysApplication.class, args);
